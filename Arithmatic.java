847abae2b51d47af8e787ec8c323d826a9621932
YashAmbildhuke <143258450+YashAmbildhuke@users.noreply.github.com>
2023-08-26 15:07:14 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,14 @@
+public class Arithmatic
+{
+   public static void main(String[] args)
+ {
+  int a=500,b=5;
+  System.out.println(a+b);
+
+  System.out.println(a-b);
+
+  System.out.println(a*b);
+
+  System.out.println(a/b);
+}
+}
